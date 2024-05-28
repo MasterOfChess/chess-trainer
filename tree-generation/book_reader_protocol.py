0b389abf711e9a27a2bef93d167d364bd3235a8d
MasterOfChess <1lukasiewiczkrzysztof@gmail.com>
2024-05-29 01:38:10 +0200

    Migrate book_reader_protocol module from asyncio to threading
    Flask is incompatible with asyncio. On top of that running BookReader
    in separate thread allows simpler implementation.

--- tree-generation/book_reader_protocol.py
+++ tree-generation/book_reader_protocol.py
@@ -1,176 +1,178 @@
-import asyncio
+"""
+This module contains the implementation of a book reader protocol.
+
+The module defines the following classes:
+- BaseCommand: Base class for commands used by the book reader agent.
+- BaseProtocol: Base class representing a protocol for interacting with a subprocess.
+- ExitCommand: Command class for exiting the book reader.
+- QuitCommand: Command class for quitting the book reader.
+- Edge: Data class representing an edge in the book reader.
+- EdgeResult: Data class representing the result of generating edges from a FEN position.
+- FromFenCommand: Command class for generating edges from a given FEN position.
+- BookReader: Class representing the book reader protocol.
+
+Example usage:
+book_reader = BookReader.popen('./book_reader', 'tree.bin')
+result = book_reader.from_fen('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
+print('\n'.join(map(str, result.edges)))
+"""
+import threading
 import queue
+import concurrent.futures as cf
 import logging
-import enum
-from typing import TypeVar, Generic, Any
+import subprocess
 import abc
+from typing import TypeVar, Generic
 import chess
 import dataclasses
 
+logging.basicConfig(format='%(asctime)s:%(threadName)s:%(message)s',
+                    level=logging.DEBUG,
+                    datefmt="%H:%M:%S")
 logger = logging.getLogger(__name__)
+
 T = TypeVar('T')
-BookProtocolT = TypeVar('BookProtocolT', bound='BookProtocol')
+ProtocolT = TypeVar('ProtocolT', bound='BaseProtocol')
 
 
-class CommandState(enum.Enum):
-    WAITING = enum.auto()
-    RUNNING = enum.auto()
-    DONE = enum.auto()
-
-
-class BaseCommand(Generic[BookProtocolT, T], metaclass=abc.ABCMeta):
+class BaseCommand(Generic[ProtocolT, T], metaclass=abc.ABCMeta):
     """Base class for commands used by the book reader agent.
 
     This class defines the common interface and behavior for all commands.
 
     Attributes:
-        state (CommandState):    The current state of the command.
-        result (asyncio.Future): The future object representing
-                                 the result of the command.
+        result (concurrent.futures.Future): The future object representing
+                                            the result of the command.
     """
 
     def __init__(self) -> None:
-        self.state = CommandState.WAITING
-        self.result: asyncio.Future[T] = asyncio.Future()
+        self.result: cf.Future[T] = cf.Future()
 
     @abc.abstractmethod
-    def start(self, book_reader: BookProtocolT) -> None:
-        """Start the command. Should set the command state to RUNNING."""
+    def start(self, protocol: ProtocolT) -> None:
+        """Start the command."""
 
     @abc.abstractmethod
-    def on_line(self, book_reader: BookProtocolT, line: str) -> None:
+    def on_line(self, protocol: ProtocolT, line: str) -> None:
         """Process the command line received from the book reader."""
 
-    def terminate_command(self, exc: Exception) -> None:
-        """Terminate the command with an exception."""
-        if not self.result.done():
-            self.state = CommandState.DONE
-            self.result.set_exception(exc)
-
     def set_done(self, value: T | None) -> None:
-        self.state = CommandState.DONE
         if not self.result.done():
             self.result.set_result(value)
 
+    def terminate(self):
+        self.result.cancel()
+
     def is_done(self) -> bool:
-        return self.state == CommandState.DONE
+        return self.result.done()
 
     def __repr__(self) -> str:
-        return (f'<{type(self).__name__}'
-                f'(state={self.state}, result={self.result}>')
+        return f'<{type(self).__name__}, result={self.result}>'
 
 
-class BookProtocol(asyncio.SubprocessProtocol):
+class BaseProtocol():
     """
-    Protocol for interacting with a book reader agent.
-
-    This protocol handles the communication between the book reader agent and the client.
-    It provides methods for adding commands, sending lines, and receiving data from the agent.
+    BaseProtocol class represents a base protocol for interacting with a subprocess.
 
     Attributes:
-        loop (asyncio.AbstractEventLoop): The event loop associated with the protocol.
-        transport (asyncio.SubprocessTransport | None): The transport used for communication.
-        output (bytearray): Buffer for storing the received output from the agent.
-        queue (queue.Queue): Queue for storing commands to be executed.
-        command (BaseCommand[BookProtocolT, Any] | None): The current command being executed.
+        proc (subprocess.Popen): The subprocess instance.
+        thread (threading.Thread): The thread used to run the protocol.
+        queue (queue.Queue): The queue used to store commands.
+        curr_command (BaseCommand | None): The current command being executed.
+        terminate_event (threading.Event): The event used to signal termination.
+        lock (threading.Lock): The lock used for thread synchronization.
+        stdin_lock (threading.Lock): The lock used for stdin synchronization.
     """
 
-    def __init__(self) -> None:
-        self.loop = asyncio.get_running_loop()
-        self.transport: asyncio.SubprocessTransport | None = None
-        self.output = bytearray()
+    def __init__(self, proc: subprocess.Popen):
+        self.proc = proc
+        self.thread = threading.Thread(target=self.run)
         self.queue: queue.Queue[BaseCommand] = queue.Queue()
-        self.command: BaseCommand[BookProtocolT, Any] | None = None
-        self.return_code: asyncio.Future[int] = asyncio.Future()
+        self.curr_command: BaseCommand | None = None
+        self.terminate_event = threading.Event()
+        self.lock = threading.Lock()
+        self.stdin_lock = threading.Lock()
+        self.thread.start()
 
-    def _update_current_command(self) -> None:
-        if self.command is None or self.command.is_done():
-            try:
-                self.command = self.queue.get()
-                self.command.start(self)
-            except queue.Empty:
-                self.command = None
+    def _update_curr_command(self):
+        with self.lock:
+            logger.debug('_update_curr_command')
+            if self.curr_command is None or self.curr_command.is_done():
+                try:
+                    self.curr_command = self.queue.get_nowait()
+                    logger.debug('Starting command: %s', self.curr_command)
+                    self.curr_command.start(self)
+                except queue.Empty:
+                    self.curr_command = None
 
-    def connection_made(self, transport: asyncio.SubprocessTransport) -> None:
-        self.transport = transport
-        logger.debug('%s: Connection made', self)
+    def send_line(self, line: str):
+        with self.stdin_lock:
+            logger.debug('%s: Send line: %s', self, line)
+            self.proc.stdin.write((line + '\n').encode('utf-8'))
+            self.proc.stdin.flush()
 
-    def pipe_data_received(self, fd: int, data: bytes) -> None:
-        logger.debug('%s: Pipe data received (fd: %s, data: %r)', self, fd,
-                     data)
-        if fd != 1:
-            return
-        self.output.extend(data)
-        lines = self.output.split(b'\n')
-        self.output = lines.pop()
-        for line in lines:
-            self._update_current_command()
-            self.loop.call_soon(self.command.on_line, self,
-                                line.decode('utf-8'))
+    def line_received(self, line: str):
+        logger.debug('%s: Received line: %s', self, line)
+        self._update_curr_command()
+        if self.curr_command is not None:
+            self.curr_command.on_line(self, line)
 
-    async def add_command(self, command: BaseCommand[BookProtocolT, T]) -> T:
+    def run(self):
+        while not self.terminate_event.is_set():
+            line = self.proc.stdout.readline().decode('utf-8')
+            if not line:
+                break
+            self.line_received(line)
+        self._clean_up()
+
+    def add_command(self, command: BaseCommand[ProtocolT, T]) -> T:
         logger.debug('%s: Command added: %s', self, command)
         self.queue.put(command)
-        self._update_current_command()
-        return await command.result
+        self._update_curr_command()
+        return command.result.result()
 
-    def send_line(self, line: str) -> None:
-        logger.debug('%s: Send line: %s', self, line)
-        stdin = self.transport.get_pipe_transport(0)
-        stdin.write((line + '\n').encode('utf-8'))
-
-    def pipe_connection_lost(self, fd: int, exc: Exception | None) -> None:
-        logger.debug('%s: Pipe connection lost (fd: %s, exc: %r)', self, fd,
-                     exc)
-
-    def process_exited(self) -> None:
-        logger.debug('%s: Child process exited', self)
-
-    def connection_lost(self, exc: Exception | None) -> None:
-        exit_code = self.transport.get_returncode()
-        logger.debug('%s: Connection lost (exc: %r, exit_code: %d)', self, exc,
-                     exit_code)
-        if self.command:
-            self.command.terminate_command(exc)
-        while True:
-            try:
-                self.queue.get_nowait().terminate_command(exc)
-            except queue.Empty:
-                break
-        self.return_code.set_result(exit_code)
+    @classmethod
+    def popen(cls, command: str, *args):
+        proc = subprocess.Popen([command, *args],
+                                stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        return cls(proc)
 
     def close(self):
-        logger.debug('%s: Closing connection', self)
-        self.transport.close()
-        self.return_code.set_result(0)
+        self.terminate_event.set()
+        return self.proc.wait()
 
-    def __repr__(self) -> str:
-        if self.transport is not None:
-            pid = self.transport.get_pid()
-        else:
-            pid = '?'
-        return f'<{type(self).__name__} (pid={pid})>'
+    def _clean_up(self):
+        while True:
+            try:
+                command = self.queue.get_nowait()
+                command.terminate()
+            except queue.Empty:
+                break
+        self.proc.terminate()
+        return self.proc.wait()
 
 
-class QuitCommand(BaseCommand[BookProtocol, None]):
+class ExitCommand(BaseCommand[BaseProtocol, None]):
 
-    def start(self, book_reader: BookProtocol) -> None:
-        self.state = CommandState.RUNNING
-        book_reader.send_line('quit')
+    def start(self, protocol: BaseProtocol):
+        logger.debug('Exitting')
+        protocol.send_line('exit')
         self.set_done(None)
 
-    def on_line(self, book_reader: BookProtocol, line: str) -> None:
+    def on_line(self, protocol: BaseProtocol, line: str):
         pass
 
 
-class ExitCommand(BaseCommand[BookProtocol, None]):
+class QuitCommand(BaseCommand[BaseProtocol, None]):
 
-    def start(self, book_reader: BookProtocol) -> None:
-        self.state = CommandState.RUNNING
-        book_reader.send_line('exit')
+    def start(self, protocol: BaseProtocol):
+        logger.debug('Quitting')
+        protocol.send_line('quit')
         self.set_done(None)
 
-    def on_line(self, book_reader: BookProtocol, line: str) -> None:
+    def on_line(self, protocol: BaseProtocol, line: str):
         pass
 
 
@@ -186,7 +188,7 @@ class EdgeResult:
     edges: list[Edge] = dataclasses.field(default_factory=list)
 
 
-class FromFenCommand(BaseCommand[BookProtocol, EdgeResult]):
+class FromFenCommand(BaseCommand[BaseProtocol, EdgeResult]):
     """
     Represents a command to generate edges from a given FEN position.
 
@@ -204,11 +206,10 @@ class FromFenCommand(BaseCommand[BookProtocol, EdgeResult]):
         self.expected_lines = None
         self.processed_lines = 0
 
-    def start(self, book_reader: BookProtocol) -> None:
-        self.state = CommandState.RUNNING
-        book_reader.send_line(f'fromfen {self.fen}')
+    def start(self, protocol: BaseProtocol) -> None:
+        protocol.send_line(f'fromfen {self.fen}')
 
-    def on_line(self, book_reader: BookProtocol, line: str) -> None:
+    def on_line(self, _: BaseProtocol, line: str) -> None:
         words = line.strip().split()
         if words[0] == 'positionmoves':
             self.expected_lines = int(words[1])
@@ -223,100 +224,40 @@ class FromFenCommand(BaseCommand[BookProtocol, EdgeResult]):
             self.set_done(self.edge_result)
 
 
-class BookReader:
-    """
-    A class representing a book reader agent.
+class BookReader(BaseProtocol):
 
-    Attributes:
-        transport (asyncio.SubprocessTransport): The transport used for communication.
-        protocol (BookProtocol): The protocol used for communication.
+    def exit(self) -> int:
+        self.add_command(ExitCommand())
+        return self.close()
 
-    Methods:
-        from_fen: Reads a book from a given FEN string.
-        quit: Sends a quit command to the book reader.
-        exit: Sends an exit command to the book reader.
-        popen: Opens a book reader process.
+    def quit(self) -> int:
+        self.add_command(QuitCommand())
+        return self.close()
 
-    """
-
-    def __init__(self, transport: asyncio.SubprocessTransport,
-                 protocol: BookProtocol) -> None:
-        self.transport = transport
-        self.protocol = protocol
-
-    async def from_fen(self, fen: str) -> EdgeResult:
-        """
-        Reads a book from a given FEN string.
-
-        Args:
-            fen (str): The FEN string representing the board position.
-
-        Returns:
-            EdgeResult: The result of the operation.
-
-        """
-        from_fen_command = FromFenCommand(fen)
-        return await self.protocol.add_command(from_fen_command)
-
-    async def quit(self) -> None:
-        """
-        Sends a quit command to the book reader.
-
-        """
-        logger.debug('Quitting')
-        quit_command = QuitCommand()
-        await self.protocol.add_command(quit_command)
-        self.protocol.close()
-
-    async def exit(self) -> None:
-        """
-        Sends an exit command to the book reader.
-
-        """
-        exit_command = ExitCommand()
-        await self.protocol.add_command(exit_command)
-        self.protocol.close()
-
-    @classmethod
-    async def popen(cls, command: str, *args):
-        """
-        Opens a book reader process.
-
-        Args:
-            command (str): The command to execute.
-            *args: Additional arguments to pass to the command.
-
-        Returns:
-            BookReader: An instance of the BookReader class.
-
-        """
-        transport, protocol = await asyncio.get_running_loop().subprocess_exec(
-            BookProtocol, command, *args)
-        return cls(transport, protocol)
+    def from_fen(self, fen: str):
+        return self.add_command(FromFenCommand(fen))
 
 
-#----------------------------------------------------------------
+#######################################################
 # Example usage
-#----------------------------------------------------------------
+#######################################################
 
 if __name__ == '__main__':
 
-    async def main():
-        # logger.setLevel(logging.INFO)
-        book_reader = await BookReader.popen('./book_reader', 'tree.bin')
+    def main():
+        book_reader = BookReader.popen('./book_reader', 'tree.bin')
         while True:
             message = input()
             if message.startswith('exit'):
-                await book_reader.exit()
+                print(book_reader.exit())
                 break
             if message.startswith('quit'):
-                await book_reader.quit()
+                print(book_reader.quit())
                 break
             if message.startswith('fromfen'):
                 fen = message.split(None, 1)[1]
-                result = await book_reader.from_fen(fen)
+                result = book_reader.from_fen(fen)
                 print('\n'.join(map(str, result.edges)))
                 continue
 
-    logging.basicConfig(level=logging.DEBUG)
-    asyncio.run(main())
+    main()
