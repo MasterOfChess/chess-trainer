24400898986a1046ba5f724263335ebdb7ae0598
MasterOfChess <1lukasiewiczkrzysztof@gmail.com>
2024-05-30 20:57:12 +0200

    Extend chessboard to handle promotion.

--- src/static/play_bot.js
+++ src/static/play_bot.js
@@ -5,8 +5,17 @@ var game = new Chess();
 var $status = $("#status");
 var $fen = $("#fen");
 var $pgn = $("#pgn");
-
+var whiteSquareGrey = '#a9a9a9';
+var blackSquareGrey = '#696969';
+const whiteSquarePink = '#FFB6C1';
+const blackSquarePink = '#FF69B4';
+let promotion_running = false;
 function onDragStart(source, piece, position, orientation) {
+  console.log("onDragStart", source, piece, position, orientation);
+  if (promotion_running) {
+    $('#myBoard .square-' + source).get(0).click();
+    console.log("Clicked " + source);
+  }
   // do not pick up pieces if the game is over
   if (game.game_over()) return false;
 
@@ -19,7 +28,87 @@ function onDragStart(source, piece, position, orientation) {
   }
 }
 
-async function onDrop(source, target) {
+function clickSquare(square) {
+  var $square = $('#myBoard .square-' + square);
+  $square.on('click', function () {
+    console.log('click');
+  });
+}
+function promotionSquaresUnder(square) {
+  let file = square[0];
+  let rank = parseInt(square[1]);
+  let squares = [];
+  let pieces = ['q', 'r', 'b', 'n'];
+  if (rank == 8) {
+    for (let i = 8; i >= 5; i--) {
+      squares.push({ coord: file + i, piece: pieces[8 - i] });
+    }
+  }
+  else {
+    for (let i = 1; i <= 4; i++) {
+      squares.push({ coord: file + i, piece: pieces[i - 1] });
+    }
+  }
+  return squares;
+}
+
+function pieceColor(piece) {
+  return piece.search(/^w/) !== -1 ? 'w' : 'b';
+}
+
+async function promotionHighlightSquares() {
+  $('.promotion-square').each(function (index) {
+    if ($(this).hasClass('black-3c85d')) {
+      $(this).css('background', blackSquarePink);
+    }
+    else {
+      $(this).css('background', whiteSquarePink);
+    }
+  });
+}
+
+function promotionRemoveHighlight() {
+  $('#myBoard .square-55d63').css('background', '')
+}
+
+async function promotionOnDrop(move, source, target, piece) {
+  if (move.promotion) {
+    let fen = game.fen();
+    let color = pieceColor(piece);
+    game.remove(source);
+    let new_piece = await new Promise((resolve) => {
+      console.log("Promoting");
+      promotion_running = true;
+      for (let square of promotionSquaresUnder(target)) {
+        let $square = $('#myBoard .square-' + square.coord);
+        game.put({ type: square.piece, color: color }, square.coord);
+        console.log("Putting " + square.piece + " at " + square.coord);
+        $square.addClass('promotion-square');
+        $square.attr('data-promotion', square.piece);
+      }
+      promotionHighlightSquares();
+
+      $('.promotion-square').on('click', function () {
+        console.log('click', $(this).attr('data-square'));
+        promotion_running = false;
+        let piece = $(this).attr('data-promotion');
+        game.load(fen);
+        // Clean up
+        promotionRemoveHighlight();
+        $('.promotion-square').off('click');
+        $('.promotion-square').removeAttr('data-promotion');
+        $('.promotion-square').removeClass('promotion-square');
+        resolve(piece);
+      });
+
+    });
+    move.promotion = new_piece;
+    console.log("Promotion done");
+  }
+}
+
+async function onDrop(source, target, piece) {
+  console.log("onDrop", source, target);
   let is_legal = (source, target) => {
     let legal_moves = game
       .moves({ square: source, verbose: true })
@@ -31,20 +120,13 @@ async function onDrop(source, target) {
   };
   let move = is_legal(source, target);
   if (move === null) return "snapback";
-
-  if (move.promotion) {
-    let piece = await new Promise((resolve) => {
-      $("#pieceSelection").show();
-      $(".promotion-piece").on("click", function () {
-        $("#pieceSelection").hide();
-        resolve($(this).data("piece"));
-      });
-    });
-    move.promotion = piece;
-  }
+  // Handle promotions
+  await promotionOnDrop(move, source, target, piece);
   game.move(move);
+  board.position(game.fen());
 
-  $.post("/make_move", { fen: game.fen() }, function (data) {
+  $.post("/make_move", { fen: game.fen() }, async function (data) {
+    await new Promise((resolve) => setTimeout(resolve, 300));
     game.load(data.fen);
     board.position(game.fen());
   });
@@ -55,10 +137,12 @@ async function onDrop(source, target) {
 // update the board position after the piece snap
 // for castling, en passant, pawn promotion
 function onSnapEnd() {
+  console.log("onSnapEnd");
   board.position(game.fen());
 }
 
 function updateStatus() {
+  console.log("Updating status");
   var status = "";
 
   var moveColor = "White";
